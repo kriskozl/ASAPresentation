766d782138a8753b99db44469aa62a937e59c9dc
kriskozl <kristianzlatanov4200@gmail.com>
2023-12-24 00:27:14 +0200

    Added overlap check func

--- scripts/image-gallery.js
+++ scripts/image-gallery.js
@@ -10,6 +10,27 @@ const handleOnUp = () => {
     track.dataset.prevPercentage = track.dataset.percentage || "0";
 };
 
+const checkOverlap = () => {
+    // Check if the image track overlaps with the text container
+    const trackRect = track.getBoundingClientRect();
+    const textRect = textContainer.getBoundingClientRect();
+
+    const overlap = !(trackRect.right < textRect.left ||
+        trackRect.left > textRect.right ||
+        trackRect.bottom < textRect.top ||
+        trackRect.top > textRect.bottom);
+    if (overlap) {
+        // If they overlap, hide the text
+        textContainer.classList.add('hidden');
+    } else {
+        // If they don't overlap, show the text
+        textContainer.classList.remove('hidden');
+    }
+
+    // Use requestAnimationFrame to continuously check for overlap
+    requestAnimationFrame(checkOverlap);
+};
+
 const handleOnMove = (e) => {
     if (track.dataset.mouseDownAt === "0") return;
 
@@ -38,22 +59,6 @@ const handleOnMove = (e) => {
             { duration: 1200, fill: "forwards" }
         );
     }
-
-    // Check if the image track overlaps with the text container
-    const trackRect = track.getBoundingClientRect();
-    const textRect = textContainer.getBoundingClientRect();
-
-    const overlap = !(trackRect.right < textRect.left ||
-        trackRect.left > textRect.right ||
-        trackRect.bottom < textRect.top ||
-        trackRect.top > textRect.bottom);
-    if (overlap) {
-        // If they overlap, hide the text
-        textContainer.classList.add('hidden');
-    } else {
-        // If they don't overlap, show the text
-        textContainer.classList.remove('hidden');
-    }
 };
 
 window.onmousedown = (e) => handleOnDown(e);
@@ -62,3 +67,6 @@ window.onmouseup = handleOnUp;
 window.ontouchend = handleOnUp;
 window.onmousemove = (e) => handleOnMove(e);
 window.ontouchmove = (e) => handleOnMove(e.touches[0]);
+
+// Start the continuous overlap check
+checkOverlap();
